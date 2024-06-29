85afc4112ad7cdbf35dc40a28462d5c87ec2956c
Daryl Bacurin <darylbacurin14@gmail.com>
2024-06-30 00:28:01 +0800

    some changes

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -1,12 +1,14 @@
-import Footer from "./Footer";
+
 import HeroImg from "./HeroImg";
 import Main from "./Main";
+import Footer from "./Footer";
 
 export default function Article(){
     return(
         <article className="b-article">
             <HeroImg/>
             <Main/>
+            <Footer/>
         </article>
     )
 }
--- src/components/Article/Footer.tsx
+++ src/components/Article/Footer.tsx
@@ -1,15 +1,25 @@
+import { useState } from 'react';
 import avatar from '../../../images/avatar-michelle.jpg';
 import share from '../../../images/icon-share.svg';
 
 export default function Footer(){
+    const [toggle, setToggle] = useState<boolean>(false);
+
     return( 
         <div className='b-footer'>
+            {toggle && <div className='b-footer-modal'>
+                <ul>
+                    <li></li>
+                </ul>
+                <button className='b-footer__share' onClick={()=>setToggle(!toggle)}><img src={share} alt="" /></button>
+
+            </div>}
             <img className="b-footer__image" src={avatar} alt="" />
             <div className='b-footer-description'>
                 <h2 className='b-footer-description__name'>Michelle Appleton</h2>
                 <p className='b-footer-description__date'>28 Jun 2020</p>
             </div>
-            <button className='b-footer__share'><img src={share} alt="" /></button>
+            <button className='b-footer__share' onClick={()=>setToggle(!toggle)}><img src={share} alt="" /></button>
         </div>
     )
 }
